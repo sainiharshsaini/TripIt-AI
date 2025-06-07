9e9a7b78e5077765a127927371e9a9065c5b39a8
sainiharshsaini <singhharshsaini7@gmail.com>
2025-06-07 16:50:56 +0530

    Added new features

--- TripIt-AI-be/dist/index.js
+++ TripIt-AI-be/dist/index.js
@@ -4,11 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const generate_1 = __importDefault(require("./routes/generate"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
-const port = 3000;
+const port = process.env.PORT || 4000;
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
-    console.log("harsh sauuu");
+    res.send("Express on Vercel");
 });
+app.use('/api', generate_1.default);
 app.listen(port, () => console.log(`Server listening on port ${port}`));
---
+++ TripIt-AI-be/dist/utils/gemini.js
@@ -0,0 +1,66 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __asyncValues = (this && this.__asyncValues) || function (o) {
+    if (!Symbol.asyncIterator) throw new TypeError("Symbol.asyncIterator is not defined.");
+    var m = o[Symbol.asyncIterator], i;
+    return m ? m.call(o) : (o = typeof __values === "function" ? __values(o) : o[Symbol.iterator](), i = {}, verb("next"), verb("throw"), verb("return"), i[Symbol.asyncIterator] = function () { return this; }, i);
+    function verb(n) { i[n] = o[n] && function (v) { return new Promise(function (resolve, reject) { v = o[n](v), settle(resolve, reject, v.done, v.value); }); }; }
+    function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateTripPlan = generateTripPlan;
+const genai_1 = require("@google/genai");
+function generateTripPlan(prompt) {
+    return __awaiter(this, void 0, void 0, function* () {
+        var _a, e_1, _b, _c;
+        const ai = new genai_1.GoogleGenAI({
+            apiKey: process.env.GOOGLE_GEMINI_AI_API_KEY,
+        });
+        const config = {
+            responseMimeType: 'application/json',
+        };
+        const model = 'gemini-1.5-flash';
+        const contents = [
+            {
+                role: 'user',
+                parts: [
+                    {
+                        text: prompt,
+                    },
+                ],
+            },
+        ];
+        const response = yield ai.models.generateContentStream({
+            model,
+            config,
+            contents,
+        });
+        let fullText = '';
+        try {
+            for (var _d = true, response_1 = __asyncValues(response), response_1_1; response_1_1 = yield response_1.next(), _a = response_1_1.done, !_a; _d = true) {
+                _c = response_1_1.value;
+                _d = false;
+                const chunk = _c;
+                if (chunk.text) {
+                    fullText += chunk.text;
+                }
+            }
+        }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
+        finally {
+            try {
+                if (!_d && !_a && (_b = response_1.return)) yield _b.call(response_1);
+            }
+            finally { if (e_1) throw e_1.error; }
+        }
+        return fullText;
+    });
+}
