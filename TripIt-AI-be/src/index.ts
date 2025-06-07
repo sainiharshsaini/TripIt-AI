4e629a5ee900581882fa206a3895ed3e2904ecf9
sainiharshsaini <singhharshsaini7@gmail.com>
2025-06-07 16:34:25 +0530

    Added new features

--- TripIt-AI-be/src/index.ts
+++ TripIt-AI-be/src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import generateRoute from './routes/generate'
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 4000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Express on Vercel")
+});
 
 app.use('/api', generateRoute);
 
